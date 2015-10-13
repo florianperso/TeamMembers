efe6e2b64c5a1c4f23ddc42c70586ec94e4ef92a
floriandubois <florian@instaply.com>
2015-10-13 15:25:36 -0700

    Handling navigation state for saving/loading on app suspension

--- SlackTeamMembers/SlackTeamMembers.WindowsPhone/MainPage.xaml.cs
+++ SlackTeamMembers/SlackTeamMembers.WindowsPhone/MainPage.xaml.cs
@@ -1,32 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Windows.UI.Xaml.Navigation;
+using SlackTeamMembers.Common;
 
 namespace SlackTeamMembers
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
+        #region Fields
+
+        private readonly NavigationHelper _navigationHelper;
+
+        #endregion
+
+        #region Constructor
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+            _navigationHelper = new NavigationHelper(this);
+            _navigationHelper.LoadState += NavigationHelper_LoadState;
+            _navigationHelper.SaveState += NavigationHelper_SaveState;
+        }
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+        #endregion
+
+        #region Navigation
+
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
         }
 
         /// <summary>
@@ -36,13 +41,16 @@ namespace SlackTeamMembers
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+            _navigationHelper.OnNavigatedTo(e);
         }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            _navigationHelper.OnNavigatedFrom(e);
+
+            base.OnNavigatedFrom(e);
+        }
+
+        #endregion
     }
 }
