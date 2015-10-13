164abec9932779042869c9a3daba9dc6a6d7df3a
floriandubois <florian@instaply.com>
2015-10-13 16:30:50 -0700

    Creating ViewModel of MainPage

---
+++ SlackTeamMembers.Tests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SlackTeamMembers.Service.Modules;
+using SlackTeamMembers.ViewModels;
+
+namespace SlackTeamMembers.Tests
+{
+    [TestClass]
+    public class MainWindowViewModelTests
+    {
+        private MainWindowViewModel _mainWindowViewModel;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _mainWindowViewModel = new MainWindowViewModel(new TeamModuleMock());
+            _mainWindowViewModel.LoadMembers().Wait();
+        }
+
+        [TestMethod]
+        public void MembersCollectionAfterDeserializingShouldNotBeNullOrEmpty()
+        {
+            Assert.IsNotNull(_mainWindowViewModel.TeamMembers);
+            Assert.IsTrue(_mainWindowViewModel.TeamMembers.Any());
+        }
+
+        [TestMethod]
+        public void MembersCollectionAfterDeserializingShouldContain7Elements()
+        {
+            Assert.IsTrue(_mainWindowViewModel.TeamMembers.Count == 7);
+        }
+    }
+}
---
+++ SlackTeamMembers/SlackTeamMembers.Shared/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using SlackTeamMembers.Service.Models;
+using SlackTeamMembers.Service.Modules;
+
+namespace SlackTeamMembers.ViewModels
+{
+    public sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private readonly ITeamModule _teamModule;
+        private ObservableCollection<TeamMember> _teamMembers;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowViewModel(ITeamModule teamModule)
+        {
+            _teamModule = teamModule;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<TeamMember> TeamMembers
+        {
+            get { return _teamMembers; }
+            set
+            {
+                if (_teamMembers == value) return;
+                _teamMembers = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task LoadMembers()
+        {
+            var members = await _teamModule.GetTeamMembersAsync();
+            if (members != null)
+                TeamMembers = new ObservableCollection<TeamMember>(members.Members);
+        }
+
+        #endregion
+
+        #region INotifiedPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
