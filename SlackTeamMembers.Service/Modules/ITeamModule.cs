e76e976d56077d1adc89e0f6dcc1bd50a3b8c091
floriandubois <florian@instaply.com>
2015-10-13 16:10:49 -0700

    Adding Modules for team members with mock

---
+++ SlackTeamMembers.Service/Modules/ITeamModule.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SlackTeamMembers.Service.Models;
+
+namespace SlackTeamMembers.Service.Modules
+{
+    public interface ITeamModule
+    {
+        Task<TeamMemberResponse> GetTeamMembersAsync();
+    }
+}
